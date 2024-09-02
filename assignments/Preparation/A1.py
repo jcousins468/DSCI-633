3bf1d447f2e950794f11440dc955ea721e07c4c1
Jonah Cousins <jonahcousins@jonahs-mbp-2.wireless.rit.edu>
2024-09-02 15:06:16 -0400

    converted to DTC

--- assignments/Preparation/A1.py
+++ assignments/Preparation/A1.py
@@ -1,4 +1,6 @@
 from sklearn.naive_bayes import GaussianNB
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
 import pandas as pd
 
 if __name__ == "__main__":
@@ -6,10 +8,10 @@ if __name__ == "__main__":
     data_train = pd.read_csv("/Users/jonahcousins/Documents/DSCI_633/DSCI-633/assignments/data/Iris_train.csv")
     
     # Explore the loaded pandas dataframe
-    # Print out the 21st training data point
-    print(data_train.loc[20])
+    # Print out the 12th training data point
+    print(data_train.loc[12])
     # Print out the column "Species"
-    print(data_train["Species"])
+    print(data_train["SepalWidthCm"] < 2.5)
     # Print out the data points with "Species" == "Iris-setosa"
     print(data_train[data_train["Species"]=="Iris-setosa"])
 
@@ -18,15 +20,16 @@ if __name__ == "__main__":
     X = data_train[independent]
     Y = data_train["Species"]
     # Train model
-    clf = GaussianNB()
-    clf.fit(X,Y)
+
+    dtc = DecisionTreeClassifier()
+    dtc.fit(X,Y)
     # Load testing data
     data_test = pd.read_csv("/Users/jonahcousins/Documents/DSCI_633/DSCI-633/assignments/data/Iris_test.csv")
     X_test = data_test[independent]
     # Predict
-    predictions = clf.predict(X_test)
+    predictions = dtc.predict(X_test)
     # Predict probabilities
-    probs = clf.predict_proba(X_test)
+    probs = dtc.predict_proba(X_test)
     # Print results
     for i,pred in enumerate(predictions):
         print("%s\t%f" %(pred,max(probs[i])))
