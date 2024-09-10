364823880699e6a0dad813f76d74214cc531d323
Jonah Cousins <jonahcousins@jonahs-mbp-2.wireless.rit.edu>
2024-09-10 15:44:00 -0400

    fixed 2 typo bugs

--- assignments/Preprocess/my_preprocess.py
+++ assignments/Preprocess/my_preprocess.py
@@ -81,7 +81,7 @@ class my_pca:
         #     self.principal_components: the top n_components principal_components
         #  Vh = the transpose of V
         U, s, Vh = svd(X)
-        self.principle_components = Vh[:self.n_components].T
+        self.principal_components = Vh[:self.n_components].T
 
 
 
