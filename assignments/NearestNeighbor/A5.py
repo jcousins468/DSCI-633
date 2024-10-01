231493288d04e3b42469fc0cf9f34dd097e96927
Jonah Cousins <jonahcousins@jonahs-mbp-2.wireless.rit.edu>
2024-10-01 10:37:44 -0400

    changed file titles, fixed one output error

--- assignments/NearestNeighbor/A5.py
+++ assignments/NearestNeighbor/A5.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 if __name__ == "__main__":
     #  Load training data
-    data_train = pd.read_csv("../data/Iris_train.csv")
+    data_train = pd.read_csv("/Users/jonahcousins/Documents/DSCI_633/DSCI-633/assignments/data/Iris_train.csv")
     # Separate independent variables and dependent variables
     independent = ["SepalLengthCm",	"SepalWidthCm",	"PetalLengthCm",	"PetalWidthCm"]
     X = data_train[independent]
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     clf = my_KNN()
     clf.fit(X,y)
     # Load testing data
-    data_test = pd.read_csv("../data/Iris_test.csv")
+    data_test = pd.read_csv("/Users/jonahcousins/Documents/DSCI_633/DSCI-633/assignments/data/Iris_test.csv")
     X_test = data_test[independent]
     # Predict
     predictions = clf.predict(X_test)
--- assignments/NearestNeighbor/my_KNN.py
+++ assignments/NearestNeighbor/my_KNN.py
@@ -17,20 +17,68 @@ class my_KNN:
         # X: pd.DataFrame, independent variables, float
         # y: list, np.array or pd.Series, dependent variables, int or str
         self.classes_ = list(set(list(y)))
-        # write your code below
+        self.X = X
+        self.y = y
         return
+
+    def dist(self,x):
+        # Calculate distances of training data to a single input data point (distances from self.X to x)
+        # Output np.array([distances to x])
+        if self.metric == "minkowski":
+            distances = np.sum(np.abs(self.X - x)**self.p, axis=1)**(1/self.p)
+
+
+        elif self.metric == "euclidean":
+            distances = np.sqrt(np.sum((self.X - x)**2, axis=1))
+
+
+        elif self.metric == "manhattan":
+            distances = np.sum(np.abs(self.X - x), axis=1)
+
+
+        elif self.metric == "cosine":
+            dot_product = np.dot(self.X, x)
+            norm_x = np.linalg.norm(x)
+            norm_X = np.linalg.norm(self.X, axis=1)
+            distances = 1 - dot_product / (norm_X * norm_x)
+        else:
+            raise Exception("Unknown criterion.")
+        return distances
+
+    def k_neighbors(self,x):
+        # Return the stats of the labels of k nearest neighbors to a single input data point (np.array)
+        # Output: Counter(labels of the self.n_neighbors nearest neighbors) e.g. {"Class A":3, "Class B":2}
+        distances = self.dist(x)
+        k_indices = np.argsort(distances)[:self.n_neighbors]
+        k_nearest_labels = [self.y.iloc[i] for i in k_indices]
+        output = Counter(k_nearest_labels)
+        return output
 
     def predict(self, X):
         # X: pd.DataFrame, independent variables, float
         # return predictions: list
-        # write your code below
+        probs = self.predict_proba(X)
+        predictions = [self.classes_[np.argmax(prob)] for prob in probs.to_numpy()]
         return predictions
 
     def predict_proba(self, X):
         # X: pd.DataFrame, independent variables, float
         # prob is a dict of prediction probabilities belonging to each categories
         # return probs = pd.DataFrame(list of prob, columns = self.classes_)
-        # write your code below
+        probs = []
+        try:
+            X_feature = X[self.X.columns]
+        except:
+            raise Exception("Input data mismatch.")
+
+        for x in X_feature.to_numpy():
+            neighbors = self.k_neighbors(x)
+            # Calculate the probability of data point x belonging to each class
+            # e.g. prob = {"2": 1/3, "1": 2/3}
+            total = sum(neighbors.values())
+            prob = {class_: count/total for class_ in self.classes_ for count in [neighbors.get(class_, 0)]}
+            probs.append(prob)
+        probs = pd.DataFrame(probs, columns=self.classes_)
         return probs
 
 
